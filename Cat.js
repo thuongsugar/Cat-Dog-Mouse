cf9cbd39026ec44aca43ca1f21817fddd32a9dd2
XuanThuong01 <phamxuanthuong2301@gmail.com>
2021-05-13 11:31:58 +0700

    Commit test

--- Cat.js
+++ Cat.js
@@ -1,6 +1,9 @@
 function Cat(){
-    this.name = 'Tom'
+    this.name = 'Tom',
+    this.eatList = [];
 }
-
+ Cat.prototype.eat = function(mouse){
+    this.eatList.push(mouse);
+ }
 console.log(new Cat())
 module.exports = Cat;
