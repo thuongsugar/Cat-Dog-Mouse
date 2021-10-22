98a8ad03d5807563a1c8c72b02035daebae350ea
XuanThuong01 <phamxuanthuong2301@gmail.com>
2021-10-22 07:37:40 +0700

    user A  edit file cat.js

--- Cat.js
+++ Cat.js
@@ -1,9 +1,9 @@
-function Cat(){
-    this.name = 'Tom',
-    this.eatList = [];
+function Cat() {
+    (this.name = "Tom"), (this.eatList = []);
 }
- Cat.prototype.eat = function(mouse){
+Cat.prototype.eat = function (mouse) {
     this.eatList.push(mouse);
- }
-console.log(new Cat())
+};
+const cat1 = new Cat();
+console.log(cat1);
 module.exports = Cat;
